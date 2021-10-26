0fd45082a93504fd757bc672f2ed127d1d3b6eeb
Filippo Gambarota <filippo.gambarota@gmail.com>
2021-10-26 09:19:55 +0200

    cleaning names

--- main_script.R
+++ main_script.R
@@ -1,21 +1,21 @@
-#  Visual Analysis --------------------------------------------------------
+#  Visual Analysis (Exp 1) --------------------------------------------------------
 
 run_script("1_outliers.R", analysis = "visual")
 run_script("2_analysis.R", analysis = "visual")
 run_script("3_exploratory.R", analysis = "visual")
 run_script("4_paper_figures_tables.R", analysis = "visual")
 
-# Auditory Analysis -------------------------------------------------------
+# Auditory Analysis (Exp 2) -------------------------------------------------------
 
 run_script("1_outliers.R", analysis = "auditory")
 run_script("2_analysis.R", analysis = "auditory")
 run_script("3_exploratory.R", analysis = "auditory")
 run_script("4_paper_figures_tables.R", analysis = "auditory")
 
-# Visual vs Auditory Analysis ---------------------------------------------
+# Visual vs Auditory Analysis (Exp 1 vs Exp 2--------------------------------------
 
 run_script("study1vs2.R", analysis = "1vs2")
 
-# Create supplementary ----------------------------------------------------
+# Create supplementary materials --------------------------------------------------
 
 rmarkdown::render("supplementary/supplementary.Rmd", quiet = T)
